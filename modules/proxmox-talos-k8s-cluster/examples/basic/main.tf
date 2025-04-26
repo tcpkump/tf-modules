8f4245db4858c3a2fdb462bdfa637c64d7e8ab76
Garrett Leber <garrett@leberra.com>
2025-04-26 12:59:03 -0400

    change how image schematic file is handled

--- modules/proxmox-talos-k8s-cluster/examples/basic/main.tf
+++ modules/proxmox-talos-k8s-cluster/examples/basic/main.tf
@@ -4,7 +4,7 @@ module "example_cluster" {
   image = {
     version        = "v1.9.5"
     update_version = "v1.9.5"
-    schematic_path = "../../image/schematic.yaml"
+    schematic_path = "image/schematic.yaml"
     # Point this to a new schematic file to update the schematic
     # update_schematic_path = "talos/image/schematic.yaml"
   }
