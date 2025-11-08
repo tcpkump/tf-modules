b23bfb4b1faa116678a770c10a45ee3fc34b82e3
Garrett Leber <garrett@leberra.com>
2025-11-08 10:36:43 -0500

    only set vip on control plane

--- modules/proxmox-talos-k8s-cluster/config.tf
+++ modules/proxmox-talos-k8s-cluster/config.tf
@@ -32,7 +32,7 @@ data "talos_machine_configuration" "this" {
       ip                 = each.value.ip
       gateway            = var.cluster.gateway
       subnet_mask        = var.cluster.subnet_mask
-      vip                = var.cluster.vip
+      vip                = each.value.machine_type == "controlplane" ? var.cluster.vip : null
     }), each.value.machine_type == "controlplane" ?
     templatefile("${path.module}/machine-config/control-plane.yaml.tftpl", {
       allow_scheduling_on_control_plane_nodes = var.cluster.allow_scheduling_on_control_plane_nodes
